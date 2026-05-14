df2f508629fcfb7aaa0e8d52eacb91c5f7512701
yxe-xiao <17708363530@163.com>
2026-05-14 13:21:43 +0800

    初始

---
+++ src/main/java/com/yxe/practice/controller/PostThumbController.java
@@ -0,0 +1,54 @@
+package com.yxe.practice.controller;
+
+import com.yxe.practice.common.BaseResponse;
+import com.yxe.practice.common.ErrorCode;
+import com.yxe.practice.common.ResultUtils;
+import com.yxe.practice.exception.BusinessException;
+import com.yxe.practice.model.dto.postthumb.PostThumbAddRequest;
+import com.yxe.practice.model.entity.User;
+import com.yxe.practice.service.PostThumbService;
+import com.yxe.practice.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
